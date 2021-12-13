cc3faead859ee9caedcc8f08ee7fb95b7e22b955
pixldemon <moritzamando@gmail.com>
2021-12-13 19:50:25 +0100

    Add comments

--- src/ivp_utils/solve.rs
+++ src/ivp_utils/solve.rs
@@ -1,10 +1,12 @@
 use super::progress_bar::update_progress_bar;
 use ndarray::{ArrayBase, Dimension, OwnedRepr};
 
+// Type aliases for cleaner function signatures
 pub type Derivative<T> = dyn Fn(&T) -> T;
 pub type Solver<T> = dyn Fn(&Derivative<T>, &[T], f32) -> (T, f32);
 type Arr<D> = ArrayBase<OwnedRepr<f64>, D>;
 
+// Solve initial value problem
 pub fn solve_ivp<D: Dimension>(
 	y0: &Arr<D>,
 	f: &Derivative<Arr<D>>,
@@ -13,20 +15,25 @@ pub fn solve_ivp<D: Dimension>(
 	dt: f32,
 ) -> (Vec<Arr<D>>, Vec<f32>) {
 	let t = t as f64;
+	// Approximate number of required steps
 	let steps = (t / dt as f64 + 1.0) as usize;
 
+	// Vec for integral values
 	let mut ys = Vec::<Arr<D>>::with_capacity(steps);
 	ys.push(y0.clone());
 
+	// Vec for time step sizes
 	let mut dts = Vec::<f32>::with_capacity(steps);
 	dts.push(0.0);
 
+	// Print initally empty progress bar to stdout
 	update_progress_bar(steps, 0, 50, "Progress:", "complete");
 
 	let mut t_solved = 0.0;
 	let mut i = 1;
 
 	while t_solved < t {
+		// Compute next integral value
 		let (next_y, dt) = solver(f, &ys[0..i], dt);
 		ys.push(next_y);
 		dts.push(dt);
@@ -34,12 +41,15 @@ pub fn solve_ivp<D: Dimension>(
 		i += 1;
 		t_solved += dt as f64;
 
+		// Every 50 steps, update progress bar
 		if i % 50 == 0 {
 			update_progress_bar(steps, i, 50, "Progress: ", "complete");
 		}
 	}
 
+	// Update progress bar to show 100% completion
 	update_progress_bar(steps, steps, 50, "Progress: ", "complete");
 
+	// Return computed integral values and corresponding step sizes
 	(ys, dts)
 }
--- src/propagate.rs
+++ src/propagate.rs
@@ -1,43 +1,59 @@
 use crate::ivp_utils;
 use crate::nbs::NBodySystemData;
 
+// Calculate gravitational acceleration based on vector to attracting body and mu
 fn a_grav(r: &ndarray::Array1<f64>, mu: f64) -> ndarray::Array1<f64> {
 	(mu / (r.dot(r).sqrt().powf(3.0))) * r
 }
 
+// Calculate value of derivative for given state
 fn calc_derivative(state: &ndarray::Array2<f64>, mus: &[f64]) -> ndarray::Array2<f64> {
 	let n = mus.len();
 
 	let mut accels = ndarray::Array2::<f64>::zeros((n, 3));
 
+	// For every body index...
 	for b1 in 0..n {
+		// Interactions with other bodies earlier in the slice have already been evaluated,
+		// as the inner loop of those bodies' iterations of the outer loop already passed over this index.
+		// For every body index greater than this one...
 		for b2 in (b1 + 1)..n {
+			// Vector from b1 to b2
 			let r = &state.slice(ndarray::s![b2, ..3]) - &state.slice(ndarray::s![b1, ..3]);
 
+			// Calc acceleration of b1 due to b2
 			let mut a1 = accels.slice_mut(ndarray::s![b1, ..]);
 			a1 += &a_grav(&r, mus[b2]);
 
+			// Calc acceleration of b2 due to b1
 			let mut a2 = accels.slice_mut(ndarray::s![b2, ..]);
 			a2 += &a_grav(&-r, mus[b1]);
 		}
 	}
+	// Derivative also includes velocities (1st derivative of position, acceleration is 2nd)
+	// Extract them from the state vectors
 	let vels = state.slice(ndarray::s![.., 3..]);
 
+	// Return derivative state vectors in the form [vx, vy, vz, ax, ay, az]
 	ndarray::concatenate(ndarray::Axis(1), &[vels.view(), accels.view()]).unwrap()
 }
 
+// Propagate N-body system
 pub fn propagate(
 	system: &NBodySystemData,
 	solver: &ivp_utils::Solver<ndarray::Array2<f64>>,
 	t: u64,
 	dt: u32,
 ) -> NBodySystemData {
+	// Clone mus (so the closure can take ownership)
 	let mus = system.mus.clone();
+	// Derivative closure
 	let f = move |state: &ndarray::Array2<f64>| calc_derivative(state, &mus);
 
 	let mut states = system.states.clone();
 	let mut dts = system.dts.clone();
 
+	// Compute new states
 	let (new_states, new_dts) = ivp_utils::solve_ivp(
 		&system.states[system.states.len() - 1],
 		&f,
@@ -46,6 +62,7 @@ pub fn propagate(
 		dt as f32,
 	);
 
+	// Combine new and old data
 	states.extend_from_slice(&new_states[1..]);
 	dts.extend_from_slice(&new_dts[1..]);
 
